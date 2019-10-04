b47c51909c10cd15042041692d67a4d49d54af97
HerAnthony <heranthony@live.com>
2019-10-04 15:51:12 -0700

    updating files to be on schedule

--- function-new.php
+++ function-new.php
@@ -1,28 +1,39 @@
 <?php
-function makeNav($conn){
-		$sql = "SELECT pagename, pagetitle FROM test.navigation";
-		$result = $conn->query($sql);
-		echo"<ul>";
-		while ($row = $result->fetch_assoc()) {
-			echo "<li><a href='" . $row['pagename'] . "'>" .$row['pagetitle'] . "</a></li>";
-		}
-		echo"</ul>";
-    }// end of makeNav function
-
-function makeContent($conn, $thisPagename){
+function makeNav($conn, $loggedIn){
+    // This creates the navigation from the navigation table
+    $sql = "SELECT pagename, pagetitle FROM test.navigation";
+    $result = $conn->query($sql);
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()){
+      echo "<li><a href='" . $row['pagename'] . "'>" .$row['pagetitle']. "</a></li>";
+    }
+    if ($loggedIn == "not logged in"){
+      echo "<li><a href='login.php'>Log In</a></li>";
+    }
+    echo "</ul>";
+  } // end of makeNav function
+  function makeContent($conn, $thisPagename){
+    /*
+     this creates the content from the content
+     table based on the supplied $thisPagename variable.
+     It will then loop through all the matching content
+     records and export those individually.
+    */
     $sql = "SELECT * FROM test.content WHERE pagename = '$thisPagename'";
     $result = $conn->query($sql);
-    while ($row = $result->fetch_assoc()){
-        echo $row['contenttitle'];
-        echo $row['content'];
-        }
-    }// end of makeContent
-    
-function makeTitle($conn, $thisPagename){
+    while($row = $result->fetch_assoc()){
+      echo $row['contenttitle'];
+      echo $row['content'];
+    }
+  } // end of makeContent
+  function makeTitle($conn, $thisPagename){
+    /*
+     This will echo out the current page title
+     from the content table based on the page name
+     in $thisPageName
+    */
     $sql = "SELECT * FROM test.content WHERE pagename = '$thisPagename'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    echo $row['pagecontent'];
-    }// end of makeContent
-
-?>
+    echo $row['pagename'];
+  }
--- index.php
+++ index.php
@@ -1,37 +1,33 @@
 <?php
 require_once("connect.php");
 require_once("function-new.php");
-$uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
+$uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH));
 $thisPagename = array_pop($uriSegments);
 if ($thisPagename == ""){
-	$thisPagename = "index";
+  $thisPagename = "index";
 }
-
-// $thisPagename = $_GET["page"] ?? "HOME";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?php makeTitle($conn, $thisPagename); ?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php // makeTitle($conn, $thisPagename); ?></title>
 </head>
 <body>
-	<nav>
-		<?php
-		makeNav($conn);
-		?>
-	</nav>
-	<section>
-		<div>
-			<?php
-			// echo $thisPagename;
-			makeContent($conn, $thisPagename);
-			?>
-		</div>
-	</section>
+  <nav>
+    <?php
+      makeNav($conn);
+     ?>
+  </nav>
+  <section>
+    <div>
+        <?php
+        //echo $thisPagename;
+       makeContent($conn, $thisPagename);
+        ?>
+    </div>
+  </section>
 </body>
 </html>
