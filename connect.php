ce5b525efd50d25493ade8b230e5fd26e2ca5079
HerAnthony <heranthony@live.com>
2019-10-07 13:07:21 -0700

    day10finish

--- connect.php
+++ connect.php
@@ -1,12 +1,16 @@
 <?php
+// this allows apache to redirect files based on .htaccess
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 $servername="localhost";
 $username = "root";
 $password = "secret";
+
 //create connection
 $conn = new mysqli($servername,$username,$password);
+
 //check connection fam, that arrow -> says inside of conn object
 if ($conn->connect_error) {
   die("No worky " . $conn->connect_error);
